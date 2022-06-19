4b45d180fb6d4b4619afd1daa3e3162033189584
JckXia <ys3xia@uwaterloo.ca>
2022-06-19 18:00:23 -0400

    Added dockerfile and db seeeds.

---
+++ server/start.sh
@@ -0,0 +1,31 @@
+# Assume working directory is ./server
+
+function enter_wkdir() {
+    cd /server;
+}
+
+function start_mysql_and_create_db() {
+    enter_wkdir;
+    sudo /etc/init.d/mysql start && mysql mysql < ./db/db_creation.sql;
+}
+
+function run_db_migrations() {
+    enter_wkdir;
+    cd ./models && python3 -m flask db migrate && python3 -m flask db upgrade;
+}
+
+function run_python_api_server() {
+    enter_wkdir;
+    python3 server.py;
+}
+
+
+function boot_strap() {
+    enter_wkdir;
+    start_mysql_and_create_db;
+    run_db_migrations;
+    run_python_api_server;
+}
+
+boot_strap;
+# sudo /etc/init.d/mysql start && mysql mysql < ./db/db_creation.sql && cd ./models && python3 -m flask db migrate && python3 -m flask db upgrade && python3 ../server.py

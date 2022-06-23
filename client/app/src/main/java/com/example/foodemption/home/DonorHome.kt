4a90740249b5459b0791453d763d49a9463db6bb
Anne Hu <46465219+annehu2@users.noreply.github.com>
2022-06-23 02:16:24 -0400

    homepage donor

---
+++ client/app/src/main/java/com/example/foodemption/home/DonorHome.kt
@@ -0,0 +1,62 @@
+package com.example.foodemption.home
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.foodemption.ui.theme.FoodemptionTheme
+import com.example.foodemption.R
+
+class DonorHome : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            FoodemptionTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier
+                        .fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Title("Anne")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun DonorHome(name: String) {
+
+    Title("Anne")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview3() {
+    FoodemptionTheme {
+        DonorHome("Android")
+    }
+}

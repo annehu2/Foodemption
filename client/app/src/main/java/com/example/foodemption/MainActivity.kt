93113ef1985d2f9d22f8df8220b896293fa7587d
arunikadhal <arunikadhal@users.noreply.github.com>
2022-06-23 20:55:45 -0400

    addind activity to manifest

--- client/app/src/main/java/com/example/foodemption/MainActivity.kt
+++ client/app/src/main/java/com/example/foodemption/MainActivity.kt
@@ -6,7 +6,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*

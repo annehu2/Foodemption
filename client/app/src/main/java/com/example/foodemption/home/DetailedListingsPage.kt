c251ca165b04bc1ff492966c7102f35130632df8
Anne Hu <46465219+annehu2@users.noreply.github.com>
2022-07-06 16:23:53 -0400

    new detailed listings

---
+++ client/app/src/main/java/com/example/foodemption/home/DetailedListingsPage.kt
@@ -0,0 +1,58 @@
+package com.example.foodemption.home
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.foodemption.DonateActivity
+import com.example.foodemption.ui.theme.FoodemptionTheme
+
+class DetailedListingsPage : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            FoodemptionTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier
+                        .fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    DetailedListingsPage(this, "Anne")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun DetailedListingsPage(context: Context, name: String) {
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier
+            .fillMaxSize()
+            .fillMaxHeight()
+    ) {
+        Spacer(Modifier.size(40.dp))
+        DetailedListing(context, "food", "4/20/2022", "this is a description test")
+        Spacer(Modifier.size(40.dp))
+        DetailedListing(context, "food", "4/20/2022", "this is a description test")
+        Spacer(Modifier.size(40.dp))
+        DetailedListing(context, "food", "4/20/2022", "this is a description test")
+        Spacer(Modifier.size(60.dp))
+    }
+}

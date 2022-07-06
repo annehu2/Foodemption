6f09187795b9e8a6131644ba016af859eb42e291
Anne Hu <46465219+annehu2@users.noreply.github.com>
2022-07-06 16:24:00 -0400

    Update HomeListings.kt

--- client/app/src/main/java/com/example/foodemption/home/HomeListings.kt
+++ client/app/src/main/java/com/example/foodemption/home/HomeListings.kt
@@ -1,16 +1,20 @@
 package com.example.foodemption.home
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
@@ -26,11 +30,15 @@ import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.foodemption.ui.theme.FoodemptionTheme
+import coil.compose.rememberAsyncImagePainter
+import com.example.foodemption.*
 import com.example.foodemption.R
+import com.example.foodemption.ui.theme.FoodemptionTheme
+import kotlin.concurrent.thread
 
 @Composable
-fun HomeListings(subTitle: String) {
+fun HomeListings(context: Context, subTitle: String, showType: Int) {
+// showType 1 - my claimed food - donor side
     Box(
         modifier = Modifier
             .width(300.dp)
@@ -48,50 +56,69 @@ fun HomeListings(subTitle: String) {
             .alpha(1f)
 
     ) {
-        Text(
-            text = subTitle,
-            textAlign = TextAlign.Start,
-            fontSize = 14.sp,
-            textDecoration = TextDecoration.None,
-            letterSpacing = 0.sp,
-            overflow = TextOverflow.Ellipsis,
-            modifier = Modifier
-                .alpha(1f),
-            color = Color(red = 0f, green = 0f, blue = 0f, alpha = 1f),
-        )
+        Row () {
+            Text(
+                text = subTitle,
+                textAlign = TextAlign.Start,
+                fontSize = 14.sp,
+                textDecoration = TextDecoration.None,
+                letterSpacing = 0.sp,
+                overflow = TextOverflow.Ellipsis,
+                modifier = Modifier
+                    .alpha(1f),
+                color = Color(red = 0f, green = 0f, blue = 0f, alpha = 1f),
+            )
+        }
         Row(
             modifier = Modifier
                 .fillMaxWidth()
         ) {
-            for (i in 1..3) {
-                Box(
-                    modifier = Modifier
-                        .width(112.dp)
-                        .height(115.dp)
-                        .padding(start = 0.dp, top = 40.dp, end = 0.dp, bottom = 0.dp)
+            if (showType == 1) {
+                val donations =
+                    remember { mutableStateOf(emptyList<FoodemptionApiClient.DonationsBodyData>()) }
 
+                LaunchedEffect(Unit) {
+                    val presenter = FoodemptionApiClient()
+                    thread {
+                        donations.value = presenter.getAllDonors()
+                    }
+                }
+
+                val donationsLen = donations.value.size
+                for (i in (donationsLen - 1) downTo 0) {
+                    Box(
+                        modifier = Modifier
+                            .width(112.dp)
+                            .height(115.dp)
+                            .padding(start = 0.dp, top = 40.dp, end = 0.dp, bottom = 0.dp)
+
+                    ) {
+                        Image(
+                            painter = rememberAsyncImagePainter(donations.value[i].image_url),
+                            contentDescription = "",
+                            alignment = Alignment.TopStart,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                        )
+                    }
+                }
+                OutlinedButton(
+                    onClick = { val intent = Intent(context, DetailedListingsPage::class.java)
+                        context.startActivity(intent) },
+                    colors = ButtonDefaults.textButtonColors(backgroundColor = Color(0xffFFFFFF)),
+                    modifier = Modifier
+                        .width(170.dp)
+                        .height(30.dp)
+                        .padding(start = 100.dp, top = 0.dp, end = 0.dp, bottom = 0.dp)
                 ) {
-                    val image: Painter = painterResource(id = R.drawable.food_placeholder)
+                    val image: Painter = painterResource(id = R.drawable.greyarrow)
                     Image(
                         painter = image,
                         contentDescription = "",
                         alignment = Alignment.TopStart,
-                        modifier = Modifier
-                            .fillMaxWidth()
                     )
                 }
             }
         }
-
-    }
-
-
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview4() {
-    FoodemptionTheme {
-        HomeListings("My Food Listings")
     }
 }

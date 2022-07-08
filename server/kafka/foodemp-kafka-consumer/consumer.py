c10f19911a9b48403b2cf5a63e43672ab38855c5
JckXia <ys3xia@uwaterloo.ca>
2022-07-07 22:39:33 -0400

    Succesful interactions between hosted kafka broker and our consumer/api

--- server/kafka/foodemp-kafka-consumer/consumer.py
+++ server/kafka/foodemp-kafka-consumer/consumer.py
@@ -1,5 +1,20 @@
 from kafka import KafkaConsumer
-consumer = KafkaConsumer('foodemp_notif_pipe', bootstrap_servers='kafka0:29090', group_id="test_group")
+configs = {
+    "bootstrap_servers":"pkc-ymrq7.us-east-2.aws.confluent.cloud:9092",
+    "security_protocol":"SASL_SSL",
+    "group_id": "test_group",
+    "sasl_mechanism":"PLAIN",
+    "sasl_plain_username":"HE4UYYYMJXI3TQIL",
+    "sasl_plain_password":"gnm8lEq6qSR3p6XBtyOXGpOudznDchigH1X7vs5Z3JWstjxMtIwDexVKLEX/Inh4"
+}
+consumer = KafkaConsumer('foodemp_notif_pipe', 
+                        bootstrap_servers="pkc-ymrq7.us-east-2.aws.confluent.cloud:9092",
+                        group_id="test_group",
+                        security_protocol="SASL_SSL",
+                        sasl_mechanism="PLAIN",
+                        sasl_plain_username="HE4UYYYMJXI3TQIL",
+                        sasl_plain_password="gnm8lEq6qSR3p6XBtyOXGpOudznDchigH1X7vs5Z3JWstjxMtIwDexVKLEX/Inh4"
+                    )
 
 for message in consumer:
     print ("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,

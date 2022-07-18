222e9d62d7a80517a62b04cc0290e0acdea811fc
arunikadhal <arunikadhal@users.noreply.github.com>
2022-07-18 18:14:59 -0400

    updates for maps

--- client/app/src/main/java/com/example/foodemption/home/DonorHome.kt
+++ client/app/src/main/java/com/example/foodemption/home/DonorHome.kt
@@ -57,27 +57,6 @@ fun DonorHome(context: Context, name: String) {
             OutlinedButton(
                 onClick = { val intent = Intent(context, SplashScreenActivity::class.java)
                     context.startActivity(intent) },
-                colors = ButtonDefaults.textButtonColors(backgroundColor = Color(0xFF9A1B11)),
-                modifier = Modifier
-                    .width(297.dp)
-                    .height(48.dp)
-                    .clip(
-                        RoundedCornerShape(
-                            topStart = 9.dp,
-                            topEnd = 9.dp,
-                            bottomStart = 9.dp,
-                            bottomEnd = 9.dp
-                        )
-                    )
-            ) {
-                Text("View Nearby Food Banks", color = Color.White, fontSize = 20.sp,)
-            }
-        }
-        Spacer(Modifier.size(30.dp))
-        Box(Modifier.padding(), Alignment.BottomCenter) {
-            OutlinedButton(
-                onClick = { val intent = Intent(context, SplashScreenActivity::class.java)
-                    context.startActivity(intent) },
                 colors = ButtonDefaults.textButtonColors(backgroundColor = Color(0xFF2A3B92)),
                 modifier = Modifier
                     .width(298.dp)

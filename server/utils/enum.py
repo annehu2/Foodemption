c2245158b133220d0fb449ad6f70f2996d5c162e
JckXia <ys3xia@uwaterloo.ca>
2022-07-18 18:00:45 -0400

    Completed redis integration

--- server/utils/enum.py
+++ server/utils/enum.py
@@ -1,4 +1,6 @@
 import os
 CUSTOMER_TYPE=1
 DONOR_TYPE=0
-MYSQL_HOST=os.getenv('MYSQL_HOST')
+MYSQL_HOST=os.getenv('MYSQL_HOST') or "localhost"
+KAFKA_TOPIC=os.getenv("KAFKA_TOPIC") or "foodemp_test_topic"
+REDIS_HOST=os.getenv("REDIS_HOST") or "localhost"

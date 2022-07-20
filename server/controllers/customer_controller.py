ff389e811cc9961771d53a0c8f0b65003c2fb506
Tanavya Dimri <tanavya.dimri@gmail.com>
2022-07-20 03:51:01 -0400

    Update api to get all pending claims and return entire food objects

--- server/controllers/customer_controller.py
+++ server/controllers/customer_controller.py
@@ -31,11 +31,12 @@ def verify_customer(currently_authenticated_user):
     return "", 200
 
 
-def get_kafka_message_for_requests(customer_uuid, pickup_time, food_uuid):
+def get_kafka_message_for_requests(customer_uuid, pickup_time, food_uuid, food_data):
     new_request_object = {
-            'message_type': 'Requests',
-            'payload':{
+        'message_type': 'Requests',
+        'payload':{
             'food_uuid': food_uuid, 
+            'data': food_data
             'customer_uuid': customer_uuid,
             'pickup_time': pickup_time,
             'message': "You have a new food claim request."  
@@ -55,7 +56,17 @@ def make_food_claim(currently_authenticated_user):
         return json.dumps({"message": "Fields are missing!"}), 400
 
     try:
-        buffer_notification_object = get_kafka_message_for_requests(customer_uuid, pickup_time, food_uuid)
+        food = get_food(food_uuid)
+        buffer_notification_object = get_kafka_message_for_requests(customer_uuid, pickup_time, food_uuid, 
+            {   
+                "uuid": food.uuid,
+                "title": food.title,
+                "image_url": food.image_url,
+                "description": food.description,
+                "best_before": food.best_before,
+                "is_claimed": food.is_claimed
+            }
+        )
         buffer_food_messages(food_uuid, json.dumps(buffer_notification_object))
         sendMessageToKafka(KAFKA_TOPIC, [json.dumps(buffer_notification_object)])
 
